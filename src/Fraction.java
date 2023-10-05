0a5536d06f327316a05537df15307dec6be69777
daoud doudou <daoud.doudou2001@gmail.com>
2023-10-05 13:28:07 +0200

    premiere envoie sur github

---
+++ src/Fraction.java
@@ -0,0 +1,19 @@
+public class Fraction {
+
+	
+	public String toString() {
+		return "Je suis une fraction.";
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("première affichage !");
+		
+		Fraction fraction=new Fraction();
+		
+		System.out.println(fraction.toString());
+
+	}
+
+}
